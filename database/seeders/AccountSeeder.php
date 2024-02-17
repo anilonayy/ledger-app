8369cd2d09caf4748c5111cfea20b32691c42aaa
anilonayy <cengizhananilonay@gmail.com>
2024-02-17 18:41:18 +0300

    Get user by email fixed for missed line

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -5,19 +5,23 @@ namespace Database\Seeders;
 use App\Models\Account;
 use App\Models\User;
 use App\Repositories\Account\AccountRepositoryInterface;
+use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AccountSeeder extends Seeder
 {
 
-    public function __construct(private readonly AccountRepositoryInterface $accountRepository) {}
+    public function __construct(
+        private readonly AccountRepositoryInterface $accountRepository,
+        private readonly UserRepositoryInterface $userRepository
+    ) {}
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $admin = User::where('email', 'anil@useinsider.com')->first();
+        $admin = $this->userRepository->getUserByEmail('anil@useinsider.com');
 
         $this->accountRepository->create([
             'user_id' => $admin->id,
